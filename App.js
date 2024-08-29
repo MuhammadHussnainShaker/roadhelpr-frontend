1ef963317c440597bc606850c3dd3da77f96611d
Muhammad Hussnain Shaker <mhussnainshaker251@gmail.com>
2024-08-29 15:33:27 +0500

    Everything is working fine in RoadHelpr. RoadHelpr is succesfully fetching data from backend.

--- App.js
+++ App.js
@@ -1,65 +1,60 @@
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-import {IP_ADDRESS} from './Constants';
+import { Button, StyleSheet, Text, View } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import { IP_ADDRESS } from './Constants.js'
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [count, setCount] = useState(1);
-
-  function incrementCount() {
-    setCount(count + 1);
-  }
+  const [todos, setTodos] = useState([])
 
   fetchData = async () => {
     try {
-      const response = await axios.get(`${IP_ADDRESS}/api`);
-      console.log(new Date().toLocaleTimeString(), ' ', response.data);
-      setTodos(response.data);
+      const response = await axios.get(`${IP_ADDRESS}/api`)
+      console.log(new Date().toLocaleTimeString(), ' ', response.data)
+      setTodos(response.data)
     } catch (error) {
-      console.error('Error fetching data: ', error);
+      console.error('Error fetching data: ', error)
       if (error.response) {
         console.log(
           'Server responded with a status code:',
-          error.response.status,
-        );
+          error.response.status
+        )
       } else if (error.request) {
         console.log(
           'Request was made but no response was received:',
-          error.request,
-        );
+          error.request
+        )
       } else {
-        console.log('Error setting up the request:', error.message);
+        console.log('Error setting up the request:', error.message)
       }
     }
-  };
+  }
 
   useEffect(() => {
-    fetchData();
-  }, [count]);
+    fetchData()
+  }, [])
 
   return (
     <View style={styles.container}>
-      <Text>Applet</Text>
+      <Text>RoadHelpr</Text>
       {todos && todos.length > 0 && (
         <Text>
-          Congratulations..., you are successfully fetching data at{' '}
+          Congratulations..., you are successfully fetched data at{' '}
           {new Date().toLocaleTimeString()}
         </Text>
       )}
       {todos.map((todo, index) => (
         <View key={index}>
-          <Text>{todo.name}</Text>
-          <Text>{todo.id}</Text>
+          <Text>
+            {todo.id} - {todo.name}
+          </Text>
         </View>
       ))}
-      <TouchableOpacity onPress={incrementCount}><Text>Refetch Data</Text></TouchableOpacity>
     </View>
-  );
-};
+  )
+}
 
-export default App;
+export default App
 
 const styles = StyleSheet.create({
-  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
-});
+  container: { flex: 1, alignItems: 'center', justifyContent: 'center' },
+})
--- ios/roadhelprTests/roadhelprTests.m
+++ ios/roadhelprTests/roadhelprTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface trelpo_frontendTests : XCTestCase
+@interface roadhelprTests : XCTestCase
 
 @end
 
-@implementation trelpo_frontendTests
+@implementation roadhelprTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
