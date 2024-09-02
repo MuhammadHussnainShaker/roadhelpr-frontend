27f25cbaf3da4dfc01f5b091fec8ab380d1e7f23
Muhammad Hussnain Shaker <mhussnainshaker251@gmail.com>
2024-09-02 19:29:07 +0500

    -mmm

--- App.js
+++ App.js
@@ -81,6 +81,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#121212',
     // backgroundColor: '#878276',
+    // backgroundColor: '#878276',
     alignItems: 'center',
     justifyContent: 'flex-end',
   },
